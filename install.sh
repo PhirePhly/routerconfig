8d8b503786ce665c064692b59094eb0c4d47fbb3
Kenneth Finnegan <kennethfinnegan2007@gmail.com>
2014-07-06 11:54:25 -0700

    Squid.conf added

--- install.sh
+++ install.sh
@@ -281,6 +281,63 @@ EOF
 
 
 # Configure squid3 proxy for 3128 and 3127 transparent
+if [ -a /etc/squid3/squid.conf ]; then
+	mv /etc/squid3/squid.conf /etc/squid3/squid.conf.bak
+fi
+
+mkdir -p /home/proxy
+chown -R proxy:proxy /home/proxy
+
+cat <<EOF >/etc/squid3/squid.conf
+
+acl entirenet src $ENTIRESUBNET
+acl securenet src $SECURESUBNET
+
+acl SSL_ports port 443
+acl Safe_ports port 80          # http
+acl Safe_ports port 21          # ftp
+acl Safe_ports port 443         # https
+acl Safe_ports port 70          # gopher
+acl Safe_ports port 210         # wais
+acl Safe_ports port 1025-65535  # unregistered ports
+acl Safe_ports port 280         # http-mgmt
+acl Safe_ports port 488         # gss-http
+acl Safe_ports port 591         # filemaker
+acl Safe_ports port 777         # multiling http
+acl CONNECT method CONNECT
+
+http_access deny !Safe_ports
+http_access deny CONNECT !SSL_ports
+http_access allow localhost manager
+http_access allow securenet manager
+http_access deny manager
+http_access allow entirenet
+http_access deny all
+
+http_port 3127 transparent
+http_port 3128
+
+cache_mem 256 MB
+maximum_object_size_in_memory 512 KB
+
+cache_dir ufs /home/proxy 30000 16 256
+maximum_object_size 128 MB
+cache_swap_low 96
+cache_swap_high 98
+
+coredump_dir /var/spool/squid3
+
+refresh_pattern ^ftp:           1440    20%     10080
+refresh_pattern ^gopher:        1440    0%      1440
+refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
+refresh_pattern (Release|Packages(.gz)*)$      0       20%     2880
+refresh_pattern .               0       20%     4320
+
+minimum_expiry_time 10 seconds
+
+dns_nameservers $LANIP
+
+EOF
 
 # Configure OLSR for wlan0 interface
 
